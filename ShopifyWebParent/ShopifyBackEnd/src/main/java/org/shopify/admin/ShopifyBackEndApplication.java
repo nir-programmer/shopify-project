31cf20612768823225e246cced2ba2733f2afcbb
nir <niritzhak10@gmail.com>
2021-08-22 11:22:05 +0300

    Update the ShopifyBackEndApplication main - add EntityScan

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/shopify/admin/ShopifyBackEndApplication.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/shopify/admin/ShopifyBackEndApplication.java
@@ -2,8 +2,10 @@ package org.shopify.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"corg.nir.shopify.common.entity", "org.nir.shopify.admin.user"})
 public class ShopifyBackEndApplication {
 
 	public static void main(String[] args) {
