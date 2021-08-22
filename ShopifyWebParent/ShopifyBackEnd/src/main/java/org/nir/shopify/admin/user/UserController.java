bd8b73635c03d9560fd415a9cfeea5c19884ea49
nir <niritzhak10@gmail.com>
2021-08-22 18:28:35 +0300

    Finish Code new User function

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserController.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserController.java
@@ -2,12 +2,14 @@ package org.nir.shopify.admin.user;
 
 import java.util.List;
 
+import org.nir.shopify.common.entity.Role;
 import org.nir.shopify.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
@@ -21,5 +23,28 @@ public class UserController {
 		model.addAttribute("listUsers", listUsers);
 		
 		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		
+		User user = new User();
+		user.setEnabled(true);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		
+		return "redirect:/users";
 	}
 }
--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserService.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserService.java
@@ -2,9 +2,11 @@ package org.nir.shopify.admin.user;
 
 import java.util.List;
 
+import org.nir.shopify.common.entity.Role;
 import org.nir.shopify.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 
 
@@ -12,9 +14,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
 	@Autowired
-	private UserRepository repo;
-
+	private UserRepository userRepo;
+	
+	@Autowired
+	private RoleRepository roleRepo;
+	
 	public List<User> listAll() {
-		return (List<User>) repo.findAll();
+		return (List<User>) userRepo.findAll();
+	}
+	
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		userRepo.save(user);
 	}
 }
