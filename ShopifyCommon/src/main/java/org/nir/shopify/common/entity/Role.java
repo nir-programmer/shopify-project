851b6da2f5b94e7a112a6de0118630479f07e9bf
nir <niritzhak10@gmail.com>
2021-08-22 12:06:30 +0300

    Update the Role Entity(note the equals overide method..)

--- ShopifyCommon/src/main/java/org/nir/shopify/common/entity/Role.java
+++ ShopifyCommon/src/main/java/org/nir/shopify/common/entity/Role.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -24,6 +25,10 @@ public class Role {
 	public Role() {
 	}
 	
+	public Role(Integer id) {
+		this.id = id;
+	}
+
 	public Role(String name) {
 		this.name = name;
 	}	
@@ -57,4 +62,35 @@ public class Role {
 		this.description = description;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [name=" + name + "]";
+	}
+
+	
 }
