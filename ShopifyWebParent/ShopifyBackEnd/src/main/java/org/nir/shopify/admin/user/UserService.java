ec50db21c03e286b5dca23260f8a8fd3bd8d9d05
nir <niritzhak10@gmail.com>
2021-08-23 11:32:34 +0300

    Update the UserService: Add the isEmailUnique() method

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserService.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserService.java
@@ -35,6 +35,12 @@ public class UserService {
 		userRepo.save(user);
 	}
 	
+	public boolean isEmailUnique(String email) {
+		User userByEmail = userRepo.getUserByEmail(email);
+
+		return userByEmail == null;
+	}
+	
 	private void encodePassword(User user) {
 		String encodedPassword = passwordEncoder.encode(user.getPassword());
 		user.setPassword(encodedPassword);
