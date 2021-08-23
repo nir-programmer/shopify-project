f3812d23dd893af37a6a006438c331159a4fd6e2
nir <niritzhak10@gmail.com>
2021-08-23 11:30:32 +0300

    Update the UserRepo: Add the getUserByEmail() method

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserRepository.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserRepository.java
@@ -1,9 +1,13 @@
 package org.nir.shopify.admin.user;
+
 import org.nir.shopify.common.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-
+	@Query("SELECT u FROM User u WHERE u.email = :email")
+	public User getUserByEmail(@Param("email") String email);
 }
