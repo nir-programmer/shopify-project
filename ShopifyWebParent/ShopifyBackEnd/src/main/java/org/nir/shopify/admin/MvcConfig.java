8bfe8c08d59c9f04943c85e356f29e4c276b4f60
nir <niritzhak10@gmail.com>
2021-08-24 16:20:02 +0300

    Update the name of the file system config var

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer 
 {
-	@Value("${os.file-system.prefix}")
+	@Value("${file-system-prefix}")
 	private String fileSystemPrefix;
 	
 
