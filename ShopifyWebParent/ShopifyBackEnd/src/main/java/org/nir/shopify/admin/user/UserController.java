94f34fcc8aea88aa8e2447997b082d518b406944
nir <niritzhak10@gmail.com>
2021-08-24 14:43:40 +0300

    Update UserController - add parameter to save the user photos

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserController.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserController.java
@@ -1,17 +1,22 @@
 package org.nir.shopify.admin.user;
 
+import java.io.IOException;
 import java.util.List;
 
-import org.nir.shopify.admin.UserNotFoundException;
-import org.nir.shopify.common.entity.Role;
-import org.nir.shopify.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.nir.shopify.admin.FileUploadUtil;
+import org.nir.shopify.admin.UserNotFoundException;
+import org.nir.shopify.common.entity.Role;
+import org.nir.shopify.common.entity.User;
 
 @Controller
 public class UserController {
@@ -42,9 +47,24 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/save")
-	public String saveUser(User user, RedirectAttributes redirectAttributes) {
-		System.out.println(user);
-		service.save(user);
+	public String saveUser(User user, RedirectAttributes redirectAttributes,
+			@RequestParam("image") MultipartFile multipartFile) throws IOException {
+		
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			User savedUser = service.save(user);
+			
+			String uploadDir = "user-photos/" + savedUser.getId();
+			
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+			
+		} else {
+			if (user.getPhotos().isEmpty()) user.setPhotos(null);
+			service.save(user);
+		}
+		
 		
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
 		
@@ -92,7 +112,7 @@ public class UserController {
 		String status = enabled ? "enabled" : "disabled";
 		String message = "The user ID " + id + " has been " + status;
 		redirectAttributes.addFlashAttribute("message", message);
-
+		
 		return "redirect:/users";
 	}
 }
