66b7930abdc585f1878325a70c15faa39e087f0c
nir <niritzhak10@gmail.com>
2021-08-24 16:06:44 +0300

    Add active profile - Setup file system prefix

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
@@ -3,12 +3,17 @@ package org.nir.shopify.admin;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig implements WebMvcConfigurer 
+{
+	@Value("${os.file-system.prefix}")
+	private String fileSystemPrefix;
+	
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -16,9 +21,10 @@ public class MvcConfig implements WebMvcConfigurer {
 		Path userPhotosDir = Paths.get(dirName);
 
 		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-
+		
+		System.err.println(">>MvcConfig: fileSystemPrefix: " + fileSystemPrefix);
 		registry.addResourceHandler("/" + dirName + "/**")
-			.addResourceLocations("file://" + userPhotosPath + "/");
+			.addResourceLocations(fileSystemPrefix + userPhotosPath + "/");
 	}
 
 }
