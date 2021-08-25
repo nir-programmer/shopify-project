62a696bf46d95b5c81e7355d2a64989090900660
nir <niritzhak10@gmail.com>
2021-08-25 14:25:12 +0300

    Update UserRepository for Filtering

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 **Shopify** is an ecomerce...
 
 The running app: [link text](http://shopify-heroku-nir-programmer.herokuapp.com/ShopifyAdmin/)
+
+![Alt text](DOCS/Diagrams/UML/Use-Case.png/path/to/image.png?raw=true "Optional Title")
+
+Class-Diagram-Check-Uniqueness-Of-User-Email.png
--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserRepository.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserRepository.java
@@ -1,9 +1,10 @@
 package org.nir.shopify.admin.user;
 
 import org.nir.shopify.common.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -14,6 +15,10 @@ public interface UserRepository extends PagingAndSortingRepository<User, Integer
 	public User getUserByEmail(@Param("email") String email);
 	
 	public Long countById(Integer id);
+	
+	@Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ',"
+			+ " u.lastName) LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
 	
 	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
 	@Modifying
