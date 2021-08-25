4c3386c7dae949360a7b99d5ba7b97c7fa0f8810
nir <niritzhak10@gmail.com>
2021-08-25 15:33:24 +0300

    Update the UserController For Exxporting User to PDF

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserController.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/UserController.java
@@ -175,4 +175,12 @@ public class UserController {
 		exporter.export(listUsers, response);
 	}
 	
+	@GetMapping("/users/export/pdf")
+	public void exportToPDF(HttpServletResponse response) throws IOException {
+		List<User> listUsers = service.listAll();
+
+		UserPdfExporter exporter = new UserPdfExporter();
+		exporter.export(listUsers, response);
+	}	
+	
 }
