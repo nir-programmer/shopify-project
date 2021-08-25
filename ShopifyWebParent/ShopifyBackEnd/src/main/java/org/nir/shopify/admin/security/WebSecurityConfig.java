27075aad08b30b97769a650e7633b9f903890830
nir <niritzhak10@gmail.com>
2021-08-25 17:49:11 +0300

    Update the WebSecurityConfig

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/security/WebSecurityConfig.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/security/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package org.nir.shopify.admin.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,7 +20,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()			
+				.loginPage("/login")
+				.usernameParameter("email")
+				.permitAll();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
 	}
 
 	
