f18aa93cad3fe0eeebc5b107d74781884f5b2599
nir <niritzhak10@gmail.com>
2021-08-25 10:08:39 +0300

    Create the Util class under the org.nir.shopify.common.util

---
+++ ShopifyCommon/src/main/java/org/nir/shopify/common/utils/Util.java
@@ -0,0 +1,57 @@
+package org.nir.shopify.common.utils;
+
+public class Util 
+	{
+	    // create enums for each operating system
+	    public enum OS {
+	        WINDOWS, LINUX, MAC, SOLARIS
+	    };
+	 
+	    public static OS getOperatingSystem()
+	    {
+	        // detecting the operating system using `os.name` System property
+	        String os = System.getProperty("os.name").toLowerCase();
+	 
+	        if (os.contains("win")) {
+	            return OS.WINDOWS;
+	        }
+	 
+	        else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+	            return OS.LINUX;
+	        }
+	 
+	        else if (os.contains("mac")) {
+	            return OS.MAC;
+	        }
+	 
+	        else if (os.contains("sunos")) {
+	            return OS.SOLARIS;
+	        }
+	 
+	        return null;
+	    }
+	 
+	    public static void main(String[] args)
+	    {
+	        switch (getOperatingSystem())
+	        {
+	            case WINDOWS:
+	                System.out.println("Windows Operating System");
+	                break;
+	            case LINUX:
+	                System.out.println("Linux Operating System");
+	                break;
+	            case MAC:
+	                System.out.println("Mac Operating System");
+	                break;
+	            case SOLARIS:
+	                System.out.println("Solaris Operating System");
+	                break;
+	            default:
+	                System.out.println("Unknown Operating System");
+	        }
+	    }
+	}
+
+
+
