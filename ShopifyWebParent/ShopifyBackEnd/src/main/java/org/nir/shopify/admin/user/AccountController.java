2f264de7141cee9ebc00e0f46afe11616c194f00
nir <niritzhak10@gmail.com>
2021-08-26 12:19:09 +0300

    Create the AccountController

---
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/user/AccountController.java
@@ -0,0 +1,62 @@
+package org.nir.shopify.admin.user;
+import java.io.IOException;
+
+import org.nir.shopify.admin.FileUploadUtil;
+import org.nir.shopify.admin.security.ShopifyUserDetails;
+import org.nir.shopify.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class AccountController {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping("/account")
+	public String viewDetails(@AuthenticationPrincipal ShopifyUserDetails loggedUser,
+			Model model) {
+		String email = loggedUser.getUsername();
+		User user = service.getByEmail(email);
+		model.addAttribute("user", user);
+		
+		return "account_form";
+		
+	}
+	
+	@PostMapping("/account/update")
+	public String saveDetails(User user, RedirectAttributes redirectAttributes,
+			@AuthenticationPrincipal ShopifyUserDetails loggedUser,
+			@RequestParam("image") MultipartFile multipartFile) throws IOException {
+		
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			User savedUser = service.updateAccount(user);
+			
+			String uploadDir = "user-photos/" + savedUser.getId();
+			
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+			
+		} else {
+			if (user.getPhotos().isEmpty()) user.setPhotos(null);
+			service.updateAccount(user);
+		}
+		
+		loggedUser.setFirstName(user.getFirstName());
+		loggedUser.setLastName(user.getLastName());
+		
+		redirectAttributes.addFlashAttribute("message", "Your account details have been updated.");
+		
+		return "redirect:/account";
+	}	
+}
