c83aa8438eb378c865e67896ae57c19a915b9574
nir <niritzhak10@gmail.com>
2021-08-27 11:48:44 +0300

    CODE CUSTOM ERROR PAGES FOR ERROR MESSAGES: 403,404 AND 500

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/security/WebSecurityConfig.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/security/WebSecurityConfig.java
@@ -45,6 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/users/**").hasAuthority("Admin")
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()			
