5f18c82cd6dff357f03d4de954259e019b75460e
nir <niritzhak10@gmail.com>
2021-08-28 12:24:57 +0300

    Update the FileUploadUtil class - add logs messages..

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/FileUploadUtil.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/FileUploadUtil.java
@@ -9,9 +9,13 @@ import java.nio.file.StandardCopyOption;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public class FileUploadUtil 
-{
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
 
+public class FileUploadUtil {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+	
 	public static void saveFile(String uploadDir, String fileName, 
 			MultipartFile multipartFile) throws IOException {
 		Path uploadPath = Paths.get(uploadDir);
@@ -37,12 +41,14 @@ public class FileUploadUtil
 					try {
 						Files.delete(file);
 					} catch (IOException ex) {
-						System.out.println("Could not delete file: " + file);
+						LOGGER.error("Could not delete file: " + file);
+//						System.out.println("Could not delete file: " + file);
 					}
 				}
 			});
 		} catch (IOException ex) {
-			System.out.println("Could not list directory: " + dirPath);
+			LOGGER.error("Could not list directory: " + dirPath);
+//			System.out.println("Could not list directory: " + dirPath);
 		}
 	}
 }
