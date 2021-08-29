7c7c31e58f45c126bc6f5aaced6b074df90ff44c
nir <niritzhak10@gmail.com>
2021-08-29 11:40:10 +0300

    CODE BRAND FORM

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/brand/BrandController.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/brand/BrandController.java
@@ -6,20 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.nir.shopify.admin.category.CategoryService;
 import org.nir.shopify.common.entity.Brand;
+import org.nir.shopify.common.entity.Category;
+
 
 @Controller
 public class BrandController {
 
 	@Autowired
-	private BrandService service;
-
+	private BrandService brandService;
+	
+	@Autowired
+	private CategoryService categoryService;
+	
 	@GetMapping("/brands")
 	public String listAll(Model model) {
-		List<Brand> listBrands = service.listAll();
+		List<Brand> listBrands = brandService.listAll();
 		model.addAttribute("listBrands", listBrands);
-
+		
 		return "brands/brands";
+	}
+	
+	@GetMapping("/brands/new")
+	public String newBrand(Model model) {
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+		
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("brand", new Brand());
+		model.addAttribute("pageTitle", "Create New Brand");
+		
+		return "brands/brand_form";		
 	}
 }
