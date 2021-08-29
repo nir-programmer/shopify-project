ada42927e276e6f23993bcb4fdd74795f70e4c80
nir <niritzhak10@gmail.com>
2021-08-29 10:55:29 +0300

    CODE GET PATH PREFIX FOR A GIVEN OS

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
@@ -12,23 +12,57 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	public void addResourceHandlers(ResourceHandlerRegistry registry) 
+	{
 		String dirName = "user-photos";
 		Path userPhotosDir = Paths.get(dirName);
 		
+		String pathPrefix = getPathPrefix();
+		System.err.println("Path Prefix : " + pathPrefix);
 		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
 		
 		
 		registry.addResourceHandler("/" + dirName + "/**")
-			.addResourceLocations("file:/" + userPhotosPath + "/");
+			.addResourceLocations(pathPrefix + userPhotosPath + "/");
 		
 		String categoryImagesDirName = "../category-images";
 		Path categoryImagesDir = Paths.get(categoryImagesDirName);
 		
 		String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
 		
+		//"file://"
 		registry.addResourceHandler("/category-images/**")
 			.addResourceLocations("file://" + categoryImagesPath + "/");		
 	}
+
+	private String getPathPrefix() 
+	{
+		 // detecting the operating system using `os.name` System property
+        String os = System.getProperty("os.name").toLowerCase();
+ 
+        if (os.contains("win")) {
+            return "file:/";
+        }
+ 
+        else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            return "file://";
+        }
+ 
+		/*
+		 * else if (os.contains("mac")) { return OS.MAC; }
+		 * 
+		 * else if (os.contains("sunos")) { return OS.SOLARIS; }
+		 */
+ 
+        return null;
+	}
+	
+	
+	// create enums for each operating system
+    public enum OS {
+        WINDOWS, LINUX, MAC, SOLARIS
+    };
+ 
+    
 
 }
