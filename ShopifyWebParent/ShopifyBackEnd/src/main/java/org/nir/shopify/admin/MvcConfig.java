0afd41291c4f8f843de5244b9260266e3497479c
nir <niritzhak10@gmail.com>
2021-08-30 11:48:43 +0300

    CODE CRUD BRANDS

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
@@ -13,31 +13,41 @@ public class MvcConfig implements WebMvcConfigurer {
 	
 
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String dirName = "user-photos";
-		Path userPhotosDir = Paths.get(dirName);
+	public void addResourceHandlers(ResourceHandlerRegistry registry) 
+	{
 
 		String pathPrefix = getPathPrefix();
 		System.err.println("Path Prefix : " + pathPrefix);
+		
+		
+		//EXPOSES USER IMAGES
+		String dirName = "user-photos";
+		Path userPhotosDir = Paths.get(dirName);
+
 
 		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
 
 		registry.addResourceHandler("/" + dirName + "/**").addResourceLocations(pathPrefix + userPhotosPath + "/");
 
+		
+		//EXPOSES CATEGORIES IMAGES
 		String categoryImagesDirName = "../category-images";
 		Path categoryImagesDir = Paths.get(categoryImagesDirName);
 
 		String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
 		
+		registry.addResourceHandler("/category-images/**").addResourceLocations(pathPrefix + categoryImagesPath + "/");
 		
+		//EXPOSES BRANDS IMAGES
 		String brandLogosDirName = "../brand-logos";
 		Path brandLogosDir = Paths.get(brandLogosDirName);
 
 		String brandLogosPath = brandLogosDir.toFile().getAbsolutePath();
 		
+		registry.addResourceHandler("/brand-logos/**").addResourceLocations(pathPrefix + brandLogosPath + "/");
 		
 		
-		registry.addResourceHandler("/category-images/**").addResourceLocations(pathPrefix + categoryImagesPath + "/");
+		
 	}
 
 	private String getPathPrefix() {
