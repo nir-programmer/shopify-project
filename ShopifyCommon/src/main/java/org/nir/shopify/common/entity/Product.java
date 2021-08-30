233f3955f50da625a642a06d7f57acf4ac1f9598
nir <niritzhak10@gmail.com>
2021-08-30 15:32:33 +0300

    CODE UNIT TESTS FOR PRODUCT REPOSITORY

--- ShopifyCommon/src/main/java/org/nir/shopify/common/entity/Product.java
+++ ShopifyCommon/src/main/java/org/nir/shopify/common/entity/Product.java
@@ -205,6 +205,12 @@ public class Product {
 	public void setBrand(Brand brand) {
 		this.brand = brand;
 	}
+	
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + "]";
+	}
+
 
 
 }
