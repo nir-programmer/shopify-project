b50bdf8e08c605e8ab18fd845fa05307adae683d
nir <niritzhak10@gmail.com>
2021-08-30 15:30:11 +0300

    CODE PRODUCT ENTITY AND REPO

---
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.nir.shopify.admin.product;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import org.nir.shopify.common.entity.Product;
+
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+}
