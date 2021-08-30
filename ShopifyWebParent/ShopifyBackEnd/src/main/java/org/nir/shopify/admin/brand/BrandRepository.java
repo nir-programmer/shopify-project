32c3e4ac905e6d46347700e3b5156b287714aa8c
nir <niritzhak10@gmail.com>
2021-08-30 15:56:23 +0300

    CODE PRODUCT OVERVIEW FORM

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/brand/BrandRepository.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/brand/BrandRepository.java
@@ -1,10 +1,11 @@
 package org.nir.shopify.admin.brand;
+import java.util.List;
+
+import org.nir.shopify.common.entity.Brand;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-
-import org.nir.shopify.common.entity.Brand;
 
 public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
 	
@@ -14,4 +15,7 @@ public interface BrandRepository extends PagingAndSortingRepository<Brand, Integ
 	
 	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
 	public Page<Brand> findAll(String keyword, Pageable pageable);
+	
+	@Query("SELECT NEW Brand(b.id, b.name) FROM Brand b ORDER BY b.name ASC")
+	public List<Brand> findAll();
 }
---
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductService.java
@@ -0,0 +1,17 @@
+package org.nir.shopify.admin.product;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.nir.shopify.common.entity.Product;
+
+@Service
+public class ProductService {
+
+	@Autowired private ProductRepository repo;
+
+	public List<Product> listAll() {
+		return (List<Product>) repo.findAll();
+	}
+}
