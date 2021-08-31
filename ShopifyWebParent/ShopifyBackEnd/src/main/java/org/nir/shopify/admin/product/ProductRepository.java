d69eb001a6ac973aa55f49f92d1425689c86cb9c
nir <niritzhak10@gmail.com>
2021-08-31 10:39:14 +0300

    CODE UPDATE PRODUCT ENABLED STATUS

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRepository.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRepository.java
@@ -1,9 +1,16 @@
 package org.nir.shopify.admin.product;
 
+import org.nir.shopify.common.entity.Product;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import org.nir.shopify.common.entity.Product;
-
-public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer>
+{
+	
 	public Product findByName(String name);
+	
+	@Query("UPDATE Product p SET p.enabled = ?2 WHERE p.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabled);	
 }
--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductService.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductService.java
@@ -2,41 +2,43 @@ package org.nir.shopify.admin.product;
 import java.util.Date;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.nir.shopify.common.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class ProductService {
 
 	@Autowired private ProductRepository repo;
-
+	
 	public List<Product> listAll() {
 		return (List<Product>) repo.findAll();
 	}
-	
 	
 	public Product save(Product product) {
 		if (product.getId() == null) {
 			product.setCreatedTime(new Date());
 		}
-
+		
 		if (product.getAlias() == null || product.getAlias().isEmpty()) {
 			String defaultAlias = product.getName().replaceAll(" ", "-");
 			product.setAlias(defaultAlias);
 		} else {
 			product.setAlias(product.getAlias().replaceAll(" ", "-"));
 		}
-
+		
 		product.setUpdatedTime(new Date());
-
+		
 		return repo.save(product);
 	}
 	
 	public String checkUnique(Integer id, String name) {
 		boolean isCreatingNew = (id == null || id == 0);
 		Product productByName = repo.findByName(name);
-
+		
 		if (isCreatingNew) {
 			if (productByName != null) return "Duplicate";
 		} else {
@@ -44,7 +46,11 @@ public class ProductService {
 				return "Duplicate";
 			}
 		}
-
+		
 		return "OK";
 	}
+	
+	public void updateProductEnabledStatus(Integer id, boolean enabled) {
+		repo.updateEnabledStatus(id, enabled);
+	}	
 }
