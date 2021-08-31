0eea0b93e32c979c12d31dbc602d59c21afcf8bd
nir <niritzhak10@gmail.com>
2021-08-31 10:56:03 +0300

    CODE UPLOAD PRODUCT IMAGES

--- ShopifyCommon/src/main/java/org/nir/shopify/common/entity/Product.java
+++ ShopifyCommon/src/main/java/org/nir/shopify/common/entity/Product.java
@@ -2,7 +2,10 @@ package org.nir.shopify.common.entity;
 
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,42 +22,45 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
+	
 	@Column(unique = true, length = 256, nullable = false)
 	private String name;
-
+	
 	@Column(unique = true, length = 256, nullable = false)
 	private String alias;
-
+	
 	@Column(length = 512, nullable = false, name = "short_description")
 	private String shortDescription;
-
+	
 	@Column(length = 4096, nullable = false, name = "full_description")
 	private String fullDescription;
-
+	
 	@Column(name = "created_time")
 	private Date createdTime;
-
+	
 	@Column(name = "updated_time")
 	private Date updatedTime;
-
+	
 	private boolean enabled;
-
+	
 	@Column(name = "in_stock")
 	private boolean inStock;
-
+	
 	private float cost;
-
+	
 	private float price;
-
+	
 	@Column(name = "discount_percent")
 	private float discountPercent;
-
+	
 	private float length;
 	private float width;
 	private float height;
 	private float weight;
-
+	
+	@Column(name = "main_image", nullable = false)
+	private String mainImage;
+		
 	@ManyToOne
 	@JoinColumn(name = "category_id")
 	private Category category;
@@ -61,6 +68,9 @@ public class Product {
 	@ManyToOne
 	@JoinColumn(name = "brand_id")	
 	private Brand brand;
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private Set<ProductImage> images = new HashSet<>();
 
 	public Integer getId() {
 		return id;
@@ -205,12 +215,29 @@ public class Product {
 	public void setBrand(Brand brand) {
 		this.brand = brand;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + "]";
 	}
 
+	public String getMainImage() {
+		return mainImage;
+	}
 
+	public void setMainImage(String mainImage) {
+		this.mainImage = mainImage;
+	}
 
+	public Set<ProductImage> getImages() {
+		return images;
+	}
+
+	public void setImages(Set<ProductImage> images) {
+		this.images = images;
+	}
+	
+	public void addExtraImage(String imageName) {
+		this.images.add(new ProductImage(imageName, this));
+	}
 }
