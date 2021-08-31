b34c587354385788940fab90a9489bb1b6fbb08c
nir <niritzhak10@gmail.com>
2021-08-31 10:44:30 +0300

    CODE DELETE PRODUCT

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductController.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductController.java
@@ -57,7 +57,23 @@ public class ProductController {
 		String status = enabled ? "enabled" : "disabled";
 		String message = "The Product ID " + id + " has been " + status;
 		redirectAttributes.addFlashAttribute("message", message);
-
+		
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/products/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") Integer id, 
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			productService.delete(id);
+			
+			redirectAttributes.addFlashAttribute("message", 
+					"The product ID " + id + " has been deleted successfully");
+		} catch (ProductNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+		}
+		
 		return "redirect:/products";
 	}	
 }
---
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package org.nir.shopify.admin.product;
+
+public class ProductNotFoundException extends Exception {
+
+	public ProductNotFoundException(String message) {
+		super(message);
+	}
+
+}
