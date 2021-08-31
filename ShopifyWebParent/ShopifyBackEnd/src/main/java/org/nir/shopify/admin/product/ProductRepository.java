8bea5f5a0b43c6e210c99d7ba3c1f9a036b169e8
nir <niritzhak10@gmail.com>
2021-08-31 10:34:33 +0300

    CODE CHECK UNIQUENESS OF A PRODUCT

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRepository.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.nir.shopify.common.entity.Product;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
-
+	public Product findByName(String name);
 }
