0ce0f3cd0d3dc9908216235c4b4c6712f17d696d
nir <niritzhak10@gmail.com>
2021-08-31 15:16:10 +0300

    CODE-PAGINATION, SORGING, ANDFILTER FOR PRODUCTS LISGING PAGE

--- ShopifyCommon/src/main/java/org/nir/shopify/common/entity/Product.java
+++ ShopifyCommon/src/main/java/org/nir/shopify/common/entity/Product.java
@@ -283,4 +283,11 @@ public class Product {
 		
 		return false;
 	}
+	@Transient
+	public String getShortName() {
+		if (name.length() > 70) {
+			return name.substring(0, 70).concat("...");
+		}
+		return name;
+	}
 }
--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRepository.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRepository.java
@@ -1,6 +1,8 @@
 package org.nir.shopify.admin.product;
 
 import org.nir.shopify.common.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -14,4 +16,11 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, I
 	public void updateEnabledStatus(Integer id, boolean enabled);
 	
 	public Long countById(Integer id);
+	
+	@Query("SELECT p FROM Product p WHERE p.name LIKE %?1% " 
+			+ "OR p.shortDescription LIKE %?1% "
+			+ "OR p.fullDescription LIKE %?1% "
+			+ "OR p.brand.name LIKE %?1% "
+			+ "OR p.category.name LIKE %?1%")
+	public Page<Product> findAll(String keyword, Pageable pageable);
 }
