ee76ec832646c7b9c16288fe70add4a49b85880b
Nir <niritzhak10@gmail.com>
2021-08-31 22:27:01 +0300

    Update MvcConfig-Copy his file!!!

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/MvcConfig.java
@@ -10,8 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-	
-
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		exposeDirectory("user-photos", registry);
@@ -19,88 +17,15 @@ public class MvcConfig implements WebMvcConfigurer {
 		exposeDirectory("../brand-logos", registry);
 		exposeDirectory("../product-images", registry);
 	}
-
-	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) 
-	{
-		//The prefix path of the current OS file system
-		String pathPrefix = getPathPrefix();
-		System.err.println("Path Prefix : " + pathPrefix);
-		
+	
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
 		Path path = Paths.get(pathPattern);
 		String absolutePath = path.toFile().getAbsolutePath();
 		
 		String logicalPath = pathPattern.replace("../", "") + "/**";
 				
 		registry.addResourceHandler(logicalPath)
-			.addResourceLocations(pathPrefix + absolutePath + "/");	
-		
-		
-	/*	//EXPOSES USER IMAGES
-		String dirName = "user-photos";
-		Path userPhotosDir = Paths.get(dirName);
-
-
-		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-
-		registry.addResourceHandler("/" + dirName + "/**").addResourceLocations(pathPrefix + userPhotosPath + "/");
-
-		
-		//EXPOSES CATEGORIES IMAGES
-		String categoryImagesDirName = "../category-images";
-		Path categoryImagesDir = Paths.get(categoryImagesDirName);
-
-		String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/category-images/**").addResourceLocations(pathPrefix + categoryImagesPath + "/");
-		
-		//EXPOSES BRANDS IMAGES
-		String brandLogosDirName = "../brand-logos";
-		Path brandLogosDir = Paths.get(brandLogosDirName);
-
-		String brandLogosPath = brandLogosDir.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/brand-logos/**").addResourceLocations(pathPrefix + brandLogosPath + "/");
-		*/
-		///////////////
-		
-		
+			.addResourceLocations("file:/" + absolutePath + "/");		
 	}
-	
-	/*
-	 * Path path = Paths.get(pathPattern); String absolutePath =
-	 * path.toFile().getAbsolutePath();
-	 * 
-	 * String logicalPath = pathPattern.replace("../", "") + "/**";
-	 * 
-	 * registry.addResourceHandler(logicalPath) .addResourceLocations("file:/" +
-	 * absolutePath + "/");
-	 */
-	
-	
-	private String getPathPrefix() {
-		// detecting the operating system using `os.name` System property
-		String os = System.getProperty("os.name").toLowerCase();
-
-		if (os.contains("win")) {
-			return "file:/";
-		}
-
-		else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
-			return "file://";
-		}
-
-		/*
-		 * else if (os.contains("mac")) { return OS.MAC; }
-		 * 
-		 * else if (os.contains("sunos")) { return OS.SOLARIS; }
-		 */
-
-		return null;
-	}
-
-	// create enums for each operating system
-	public enum OS {
-		WINDOWS, LINUX, MAC, SOLARIS
-	};
 
 }
