8b8fc5379974ebd601b4d77efc31276c7582e2ea
nir <niritzhak10@gmail.com>
2021-08-31 10:25:59 +0300

    CODE SAVE PRODUCT

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductService.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductService.java
@@ -1,10 +1,10 @@
 package org.nir.shopify.admin.product;
+import java.util.Date;
 import java.util.List;
 
+import org.nir.shopify.common.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import org.nir.shopify.common.entity.Product;
 
 @Service
 public class ProductService {
@@ -13,5 +13,23 @@ public class ProductService {
 
 	public List<Product> listAll() {
 		return (List<Product>) repo.findAll();
+	}
+	
+	
+	public Product save(Product product) {
+		if (product.getId() == null) {
+			product.setCreatedTime(new Date());
+		}
+
+		if (product.getAlias() == null || product.getAlias().isEmpty()) {
+			String defaultAlias = product.getName().replaceAll(" ", "-");
+			product.setAlias(defaultAlias);
+		} else {
+			product.setAlias(product.getAlias().replaceAll(" ", "-"));
+		}
+
+		product.setUpdatedTime(new Date());
+
+		return repo.save(product);
 	}
 }
