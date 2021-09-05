c8f777c8ea761b467719b0ddc3e033fceaea2241
Nir <niritzhak10@gmail.com>
2021-09-05 10:51:57 +0300

    CODE UPDATE PRODUCT QUANTITY

--- ShopifyWebParent/ShopifyFrontEnd/src/main/java/org/nir/shopify/shoppingcart/ShoppingCartService.java
+++ ShopifyWebParent/ShopifyFrontEnd/src/main/java/org/nir/shopify/shoppingcart/ShoppingCartService.java
@@ -2,27 +2,32 @@ package org.nir.shopify.shoppingcart;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.nir.shopify.common.entity.CartItem;
 import org.nir.shopify.common.entity.Customer;
 import org.nir.shopify.common.entity.Product;
+import org.nir.shopify.product.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class ShoppingCartService {
 
 	@Autowired private CartItemRepository cartRepo;
-
+	@Autowired private ProductRepository productRepo;
+	
 	public Integer addProduct(Integer productId, Integer quantity, Customer customer) 
 			throws ShoppingCartException {
 		Integer updatedQuantity = quantity;
 		Product product = new Product(productId);
-
+		
 		CartItem cartItem = cartRepo.findByCustomerAndProduct(customer, product);
-
+		
 		if (cartItem != null) {
 			updatedQuantity = cartItem.getQuantity() + quantity;
-
+			
 			if (updatedQuantity > 5) {
 				throw new ShoppingCartException("Could not add more " + quantity + " item(s)"
 						+ " because there's already " + cartItem.getQuantity() + " item(s) "
@@ -33,15 +38,22 @@ public class ShoppingCartService {
 			cartItem.setCustomer(customer);
 			cartItem.setProduct(product);
 		}
-
+		
 		cartItem.setQuantity(updatedQuantity);
-
+		
 		cartRepo.save(cartItem);
-
+		
 		return updatedQuantity;
 	}
 	
 	public List<CartItem> listCartItems(Customer customer) {
 		return cartRepo.findByCustomer(customer);
+	}
+	
+	public float updateQuantity(Integer productId, Integer quantity, Customer customer) {
+		cartRepo.updateQuantity(quantity, customer.getId(), productId);
+		Product product = productRepo.findById(productId).get();
+		float subtotal = product.getDiscountPrice() * quantity;
+		return subtotal;
 	}
 }
