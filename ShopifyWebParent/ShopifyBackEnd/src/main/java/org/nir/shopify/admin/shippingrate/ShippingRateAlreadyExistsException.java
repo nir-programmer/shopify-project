63147ae46ae08c16ddecc1725f07713b584b1892
Nir <niritzhak10@gmail.com>
2021-09-08 19:12:26 +0300

    CODE SHOPPING RATES MODULE MANAGEMENT

---
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/shippingrate/ShippingRateAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package org.nir.shopify.admin.shippingrate;
+
+public class ShippingRateAlreadyExistsException extends Exception 
+{
+
+	public ShippingRateAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
---
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/shippingrate/ShippingRateNotFoundException.java
@@ -0,0 +1,9 @@
+package org.nir.shopify.admin.shippingrate;
+
+public class ShippingRateNotFoundException extends Exception {
+
+	public ShippingRateNotFoundException(String message) {
+		super(message);
+	}
+
+}
