a1dbffd2bf39cf0251368a677c53d08d1c88ca64
Nir <niritzhak10@gmail.com>
2021-09-09 10:28:41 +0300

    Code Delete Order

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/order/OrderService.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/order/OrderService.java
@@ -51,4 +51,13 @@ public class OrderService {
 			throw new OrderNotFoundException("Could not find any orders with ID " + id);
 		}
 	}
+	
+	public void delete(Integer id) throws OrderNotFoundException {
+		Long count = repo.countById(id);
+		if (count == null || count == 0) {
+			throw new OrderNotFoundException("Could not find any orders with ID " + id); 
+		}
+
+		repo.deleteById(id);
+	}	
 }
