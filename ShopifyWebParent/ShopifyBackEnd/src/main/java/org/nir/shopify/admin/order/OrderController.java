27bdbfb887dc68b3b28fda69adfefda369d1c6e7
Nir <niritzhak10@gmail.com>
2021-09-10 18:09:26 +0300

    CODE SAVE ORDER TO DATABASE

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/order/OrderController.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/order/OrderController.java
@@ -1,26 +1,36 @@
 package org.nir.shopify.admin.order;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
+
 
 import org.nir.shopify.admin.paging.PagingAndSortingHelper;
 import org.nir.shopify.admin.paging.PagingAndSortingParam;
 import org.nir.shopify.admin.setting.SettingService;
 import org.nir.shopify.common.entity.Country;
 import org.nir.shopify.common.entity.order.Order;
+import org.nir.shopify.common.entity.order.OrderDetail;
+import org.nir.shopify.common.entity.order.OrderStatus;
+import org.nir.shopify.common.entity.order.OrderTrack;
+import org.nir.shopify.common.entity.product.Product;
 import org.nir.shopify.common.entity.setting.Setting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class OrderController {
 	private String defaultRedirectURL = "redirect:/orders/page/1?sortField=orderTime&sortDir=desc";
-
+	
 	@Autowired private OrderService orderService;
 	@Autowired private SettingService settingService;
 
@@ -28,7 +38,7 @@ public class OrderController {
 	public String listFirstPage() {
 		return defaultRedirectURL;
 	}
-
+	
 	@GetMapping("/orders/page/{pageNum}")
 	public String listByPage(
 			@PagingAndSortingParam(listName = "listOrders", moduleURL = "/orders") PagingAndSortingHelper helper,
@@ -37,19 +47,18 @@ public class OrderController {
 
 		orderService.listByPage(pageNum, helper);
 		loadCurrencySetting(request);
-
+		
 		return "orders/orders";
 	}
-
+	
 	private void loadCurrencySetting(HttpServletRequest request) {
 		List<Setting> currencySettings = settingService.getCurrencySettings();
-
+		
 		for (Setting setting : currencySettings) {
 			request.setAttribute(setting.getKey(), setting.getValue());
 		}	
 	}	
 	
-
 	@GetMapping("/orders/detail/{id}")
 	public String viewOrderDetails(@PathVariable("id") Integer id, Model model, 
 			RedirectAttributes ra, HttpServletRequest request) {
@@ -57,13 +66,13 @@ public class OrderController {
 			Order order = orderService.get(id);
 			loadCurrencySetting(request);			
 			model.addAttribute("order", order);
-
+			
 			return "orders/order_details_modal";
 		} catch (OrderNotFoundException ex) {
 			ra.addFlashAttribute("message", ex.getMessage());
 			return defaultRedirectURL;
 		}
-
+		
 	}
 	
 	@GetMapping("/orders/delete/{id}")
@@ -74,7 +83,7 @@ public class OrderController {
 		} catch (OrderNotFoundException ex) {
 			ra.addFlashAttribute("message", ex.getMessage());
 		}
-
+		
 		return defaultRedirectURL;
 	}
 	
@@ -83,19 +92,105 @@ public class OrderController {
 			HttpServletRequest request) {
 		try {
 			Order order = orderService.get(id);;
-
+			
 			List<Country> listCountries = orderService.listAllCountries();
-
+			
 			model.addAttribute("pageTitle", "Edit Order (ID: " + id + ")");
 			model.addAttribute("order", order);
 			model.addAttribute("listCountries", listCountries);
-
+			
 			return "orders/order_form";
-
+			
 		} catch (OrderNotFoundException ex) {
 			ra.addFlashAttribute("message", ex.getMessage());
 			return defaultRedirectURL;
 		}
-
+		
 	}	
+	
+	@PostMapping("/order/save")
+	public String saveOrder(Order order, HttpServletRequest request, RedirectAttributes ra) {
+		String countryName = request.getParameter("countryName");
+		order.setCountry(countryName);
+		
+		updateProductDetails(order, request);
+		updateOrderTracks(order, request);
+
+		orderService.save(order);		
+		
+		ra.addFlashAttribute("message", "The order ID " + order.getId() + " has been updated successfully");
+		
+		return defaultRedirectURL;
+	}
+
+	private void updateOrderTracks(Order order, HttpServletRequest request) {
+		String[] trackIds = request.getParameterValues("trackId");
+		String[] trackStatuses = request.getParameterValues("trackStatus");
+		String[] trackDates = request.getParameterValues("trackDate");
+		String[] trackNotes = request.getParameterValues("trackNotes");
+		
+		List<OrderTrack> orderTracks = order.getOrderTracks();
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+		
+		for (int i = 0; i < trackIds.length; i++) {
+			OrderTrack trackRecord = new OrderTrack();
+			
+			Integer trackId = Integer.parseInt(trackIds[i]);
+			if (trackId > 0) {
+				trackRecord.setId(trackId);
+			}
+			
+			trackRecord.setOrder(order);
+			trackRecord.setStatus(OrderStatus.valueOf(trackStatuses[i]));
+			trackRecord.setNotes(trackNotes[i]);
+			
+			try {
+				trackRecord.setUpdatedTime(dateFormatter.parse(trackDates[i]));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			orderTracks.add(trackRecord);
+		}
+	}
+
+	private void updateProductDetails(Order order, HttpServletRequest request) {
+		String[] detailIds = request.getParameterValues("detailId");
+		String[] productIds = request.getParameterValues("productId");
+		String[] productPrices = request.getParameterValues("productPrice");
+		String[] productDetailCosts = request.getParameterValues("productDetailCost");
+		String[] quantities = request.getParameterValues("quantity");
+		String[] productSubtotals = request.getParameterValues("productSubtotal");
+		String[] productShipCosts = request.getParameterValues("productShipCost");
+		
+		Set<OrderDetail> orderDetails = order.getOrderDetails();
+		
+		for (int i = 0; i < detailIds.length; i++) {
+			System.out.println("Detail ID: " + detailIds[i]);
+			System.out.println("\t Prodouct ID: " + productIds[i]);
+			System.out.println("\t Cost: " + productDetailCosts[i]);
+			System.out.println("\t Quantity: " + quantities[i]);
+			System.out.println("\t Subtotal: " + productSubtotals[i]);
+			System.out.println("\t Ship cost: " + productShipCosts[i]);
+			
+			OrderDetail orderDetail = new OrderDetail();
+			Integer detailId = Integer.parseInt(detailIds[i]);
+			if (detailId > 0) {
+				orderDetail.setId(detailId);
+			}
+			
+			orderDetail.setOrder(order);
+			orderDetail.setProduct(new Product(Integer.parseInt(productIds[i])));
+			orderDetail.setProductCost(Float.parseFloat(productDetailCosts[i]));
+			orderDetail.setSubtotal(Float.parseFloat(productSubtotals[i]));
+			orderDetail.setShippingCost(Float.parseFloat(productShipCosts[i]));
+			orderDetail.setQuantity(Integer.parseInt(quantities[i]));
+			orderDetail.setUnitPrice(Float.parseFloat(productPrices[i]));
+			
+			orderDetails.add(orderDetail);
+			
+		}
+		
+	}
+	
 }
