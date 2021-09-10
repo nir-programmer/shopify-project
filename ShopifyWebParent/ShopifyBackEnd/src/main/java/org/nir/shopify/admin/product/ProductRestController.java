c3a1de41489052ed5335c929b6c0935bc8c9c35e
Nir <niritzhak10@gmail.com>
2021-09-10 16:54:16 +0300

    CODE ADD PRODUCTS TO ORDER PART 2

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRestController.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/product/ProductRestController.java
@@ -1,6 +1,10 @@
 package org.nir.shopify.admin.product;
 
+import org.nir.shopify.common.entity.product.Product;
+import org.nir.shopify.common.exception.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,12 @@ public class ProductRestController {
 	public String checkUnique(Integer id, String name) {
 		return service.checkUnique(id, name);
 	}	
+	
+	@GetMapping("/products/get/{id}")
+	public ProductDTO getProductInfo(@PathVariable("id") Integer id) 
+			throws ProductNotFoundException {
+		Product product = service.get(id);
+		return new ProductDTO(product.getName(), product.getMainImagePath(), 
+				product.getDiscountPrice(), product.getCost());
+	}
 }
