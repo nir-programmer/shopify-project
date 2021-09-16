bbfad026a083057657a6a6ea276b912ecfdd82e6
Nir <niritzhak10@gmail.com>
2021-09-16 10:54:21 +0300

    CODE AMAZON S3 UTILITY CLASS

--- ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/FileUploadUtil.java
+++ ShopifyWebParent/ShopifyBackEnd/src/main/java/org/nir/shopify/admin/FileUploadUtil.java
@@ -7,8 +7,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
---
+++ ShopifyWebParent/ShopifyBackEnd/src/test/java/org/nir/shopify/admin/AmazonS3UtilTests.java
@@ -0,0 +1,84 @@
+package org.nir.shopify.admin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class AmazonS3UtilTests
+{
+	@Test
+	public void testListFolder()
+	{
+		//Given
+		//String folderName = "product-images/18"; 
+		String folderName = "test-upload"; 
+		
+		//WHEN
+		List<String> listKeys = AmazonS3Util.listFolder(folderName);
+		
+		
+		//THEN
+		assertThat(listKeys).isNotNull();
+		assertThat(listKeys.size()).isGreaterThan(0);
+		
+		listKeys.forEach(System.out::println);
+		
+	}
+	
+	@Test
+	public void testUploadFile() throws FileNotFoundException
+	{
+		
+		//GIVEN
+		
+		String folderName = "test-upload/one/two/three";
+		String fileName = "test.jpeg"; 
+		String filePath = "/home/nir/Desktop/" + fileName;
+		
+		InputStream inputStream = new FileInputStream(filePath);
+		
+		//WHEN
+		AmazonS3Util.uploadFile(folderName, fileName, inputStream);
+		
+		//THEN
+		
+	}
+	
+	@Test 
+	public void testDeleteFile()
+	{
+		//GIVEN
+		//The Key on AWS for this object : test-upload/one/two/three/test.jpeg
+		String fileName = "test-upload/one/two/three/test.jpeg"; 
+		
+		//WHEN
+		AmazonS3Util.deleteFile(fileName);
+		
+		
+		//THEN
+		
+		
+	}
+	
+	@Test
+	public void testRemoveFolder()
+	{
+		//GIVEN
+		String folderName = "test-upload"; 
+		
+		//WHEN
+		AmazonS3Util.removeFolder(folderName);
+		
+		
+		
+		
+		//THEN
+		
+	}
+
+}
